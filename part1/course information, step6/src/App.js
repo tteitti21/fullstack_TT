9ce3bcc41243194a65428e3137181c0890d2d102
Tino <tino.teittinen@edu.turkuamk.fi>
2022-12-15 22:40:03 +0200

    part1 step6 added

---
+++ part1/course information, step6/src/App.js
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleGoodClick = () => {
+    setGood(good+1)
+  }
+  const handleNeutralClick = () => setNeutral(neutral+1)
+  const handleBadClick = () => setBad(bad+1)
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button handleClick={handleGoodClick} text='Good' />
+      <Button handleClick={handleNeutralClick} text='Neutral' />
+      <Button handleClick={handleBadClick} text='Bad' />
+
+      <h1>statistics</h1>
+      <div>good {good} </div>
+      <div>neutral {neutral} </div>
+      <div>bad {bad} </div>
+    </div>
+  )
+}
+
+export default App
