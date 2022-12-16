6197497865ff129381e85816a997108670d5099c
Tino Teittinen <tino.teittinen@edu.turkuamk.fi>
2022-12-16 15:46:12 +0200

    part1 step10 added

--- part1/course information, step9/src/App.js
+++ part1/course information, step9/src/App.js
@@ -6,26 +6,26 @@ const Button = ({ handleClick, text }) => (
   </button>
 )
 
-const Show = (props) => (
-  <>
-    <div>{props.category.good.name} {props.category.good.state}</div>
-    <div>{props.category.neutral.name} {props.category.neutral.state}</div>
-    <div>{props.category.bad.name} {props.category.bad.state}</div>
-  </>
-)
-
 const Statistics = ({ good, neutral, bad}) => {
   let all = good+neutral+bad
   let average = good/(good+neutral+bad)*100
   
   return (
       <>
+        <StatisticLine text='good' value={ good } />
+        <StatisticLine text='neutral' value={ neutral } />
+        <StatisticLine text='bad' value={ bad } />
+
         <div>all {(good+neutral+bad)}</div>
         <div>average { (good-bad)/all }</div>
         <div>positive { average.toFixed(14) + ' %' }</div>
       </>
   )
 }
+
+const StatisticLine = ({ text, value }) => (
+  <div>{ text } { value }</div>
+)
 
 const App = () => {
   // save clicks of each button to its own state
@@ -38,21 +38,6 @@ const App = () => {
   }
   const handleNeutralClick = () => setNeutral(neutral+1)
   const handleBadClick = () => setBad(bad+1)
-
-  const Data = {
-    good :{
-      name: 'good',
-      state: good,
-    },
-    neutral :{
-      name: 'neutral',
-      state: neutral,
-    },    
-    bad :{
-      name: 'bad',
-      state: bad,
-    },
-  }
 
   if (good === 0 && neutral === 0 && bad === 0) {
   return (
@@ -75,7 +60,6 @@ const App = () => {
       <Button handleClick={handleBadClick} text='Bad' />
 
       <h1>statistics</h1>
-      <Show category={Data} />
       <Statistics good={good} neutral={neutral} bad={bad}/>
     </div>
     )
