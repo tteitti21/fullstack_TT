86458f79664dad5c6a191e2c6b90061621e743f4
Tino <tino.teittinen@edu.turkuamk.fi>
2022-12-16 11:06:21 +0200

    part1 step7-8 added

--- part1/course information, step7+8/src/App.js
+++ part1/course information, step7+8/src/App.js
@@ -6,6 +6,27 @@ const Button = ({ handleClick, text }) => (
   </button>
 )
 
+const Show = (props) => (
+  <>
+    <div>{props.category.good.name} {props.category.good.state}</div>
+    <div>{props.category.neutral.name} {props.category.neutral.state}</div>
+    <div>{props.category.bad.name} {props.category.bad.state}</div>
+  </>
+)
+
+const Statistics = ({ good, neutral, bad}) => {
+  let all = good+neutral+bad
+  let average = good/(good+neutral+bad)*100
+  
+  return (
+  <>
+    <div>all {(good+neutral+bad)}</div>
+    <div>average { (good-bad)/all }</div>
+    <div>positive { average.toFixed(14) + ' %' }</div>
+  </>
+  )
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
@@ -33,9 +54,6 @@ const App = () => {
     },
   }
 
-  const Show = (props) => (
-    <div>{props.category.name} {props.category.state}</div>
-  )
 
   return (
     <div>
@@ -45,10 +63,8 @@ const App = () => {
       <Button handleClick={handleBadClick} text='Bad' />
 
       <h1>statistics</h1>
-      <Show category={Data.good} />
-      <Show category={Data.neutral} />
-      <Show category={Data.bad} />
-
+      <Show category={Data} />
+      <Statistics good={good} neutral={neutral} bad={bad}/>
     </div>
   )
 }
