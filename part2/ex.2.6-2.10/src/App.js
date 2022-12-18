2422fe91b85b573657a225a6bc7ada4462673405
Tino <tino.teittinen@edu.turkuamk.fi>
2022-12-18 18:21:03 +0200

    ex.2.9 - 2.10 added

--- part2/ex.2.6-2.10/src/App.js
+++ part2/ex.2.6-2.10/src/App.js
@@ -1,12 +1,19 @@
 import { useState } from 'react'
+import SearchField from './components/SearchField'
+import FormField from './components/FormField'
+import FilterSearch from './components/FilterSearch'
 
 const App = () => {
 
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [searchField, setSearchField] = useState('')
 
   /** Adds persons to phonebook if similar name doesnt exist */
   const addPerson = (event) => {
@@ -37,29 +44,20 @@ const App = () => {
   const handleNumberChange = (event) => {
     setNewNumber(event.target.value)
   }
+  const handleSearchChange = (event) => {
+    setSearchField(event.target.value)
+  }
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addPerson}>
-        <div>
-          name: <input 
-          value={newName}
-          onChange={handleNameChange}
-          />
-        </div>
-        <div>
-          number: <input 
-          value={newNumber}
-          onChange={handleNumberChange}
-          />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
+      <SearchField searchField={searchField} handleSearchChange={handleSearchChange}/>
+      <h2>add a new</h2>
+      <FormField addPerson={addPerson} newName={newName} newNumber={newNumber} 
+        handleNameChange={handleNameChange} handleNumberChange={handleNumberChange}
+        />
       <h2>Numbers</h2>
-      ...
+      <FilterSearch persons={persons} searchField={searchField} />
     </div>
   )
 }
